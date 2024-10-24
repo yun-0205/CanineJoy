a2a2336539ed401ea5f5802432e888fa61c0cfb4
Nicole <nicole.chang@keyxentic.com>
2024-10-24 10:27:19 +0800

    feat: Add click functionality for the shopping cart

--- products.js
+++ products.js
@@ -48,3 +48,79 @@ document.addEventListener('click', function(event) {
     hideMenu();
   }
 });
+
+const products = [
+  {
+    img: "/icon/products/VitalPaws Formula.png",
+    name: "VitalPaws Formula",
+    orginPrice: 899,
+    discountPrice: 599,
+  },
+  {
+    img: "/icon/products/NatureBite Blend.png",
+    name: "NatureBite Blend",
+    orginPrice: 1200,
+    discountPrice: 1090,
+  },
+  {
+    img: "/icon/products/HealthyTails Select.png",
+    name: "HealthyTails Select",
+    orginPrice: 1080,
+    discountPrice: 690,
+  },
+  {
+    img: "/icon/products/PureLife Kibble.png",
+    name: "PureLife Kibble",
+    orginPrice: 799,
+    discountPrice: 750,
+  },
+  {
+    img: "/icon/products/ActivePaws Nutrition.png",
+    name: "ActivePaws Nutrition",
+    orginPrice: 1390,
+    discountPrice: 890,
+  },
+  {
+    img: "/icon/products/WholesomeBite Recipe.png",
+    name: "WholesomeBite Recipe",
+    orginPrice: 999,
+    discountPrice: 690,
+  },
+]
+
+let html = "";
+for (let i = 0; i < products.length; i++) {
+  html += `        
+      <div>
+        <img class="products-img" src="${products[i].img}" alt="Products img">
+        <h2>${products[i].name}</h2>
+        <div class="products-price-bag">
+          <div class="products-price-title">
+            <p>Price</p>
+            <div class="products-price">
+              <h6>$${products[i].discountPrice}</h6>
+              <p><del>$${products[i].orginPrice}</del></p>
+            </div>
+          </div>
+          <img class="add-cart-btn" src="/icon/bag.svg" alt="Shopping Bag">
+        </div>
+      </div>
+      `;
+}
+
+document.querySelector(".products-item").innerHTML = html;
+
+
+const cartBadge = document.querySelector('.cart-badge');
+let cartCount = 0;
+
+document.querySelectorAll('.add-cart-btn').forEach(img => {
+  img.addEventListener('click', () => {
+    cartCount++;
+    cartBadge.textContent = cartCount;
+
+    if (cartCount > 0) {
+      cartBadge.style.display = 'block';
+    }
+  });
+});
